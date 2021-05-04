1ee5318cc9fbd7c9bfe409051756392025f053aa
Joseph <Josephsipiorski@gmail.com>
2021-05-04 15:57:10 -0500

    completed 03_parties_spec

--- jbuilder_prac/config/routes.rb
+++ jbuilder_prac/config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
     resources :guests, only: [:show, :index] do 
       resources :gifts, only: [:index]
     end
+
+    resources :parties, only: [:show, :index]
   end 
 
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
